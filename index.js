868345c928efc38c76a34121a691411ccaf56df4
nahidraz23 <nzamanraz@gmail.com>
2025-02-27 14:00:21 +0600

    cors updated

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors(
   {
-    origin: ['https://web-mcash.vercel.app', 'http://localhost:5173'],
+    origin: ['http://localhost:5173'],
     credentials: true
   }
 ));
