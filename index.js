b011a14624c4cef35878366f4df183571796e665
nahidraz23 <nzamanraz@gmail.com>
2025-02-27 11:09:31 +0600

    fix: cors origin updated

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://mcash-server.vercel.app'],
     credentials: true
   })
 )
