3ab04318ac40032e0e299e106f066617353f1504
nahidraz23 <nzamanraz@gmail.com>
2025-02-27 13:36:29 +0600

    fix: cors options updated

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors(
   {
-    origin: ['https://web-mcash.vercel.app', 'http://localhost:5173', 'https://mcash-server.vercel.app/'],
+    origin: ['https://web-mcash.vercel.app', 'http://localhost:5173', 'https://mcash-server.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
